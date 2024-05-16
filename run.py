0d351dbd12416d98cf64893403b66eb0d5b355af
codebyMai <158331419+codebyMai@users.noreply.github.com>
2024-05-16 16:12:12 +0100

    add title and difficulty level selection

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Libraries and imports
+"""
+import os, sys, time
+import pyfiglet
+
+def typingPrint(text):
+    """
+    Typing effect for text.
+    """
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+def welcome_screen():
+    """
+    Prints title of the game and asks user to choose difficulty level.
+    """
+
+    title = pyfiglet.figlet_format('Escape',font= 'doom')
+    typingPrint(title)    
+
+    print('Warning! This game is going to make you face your deepest fears...')
+    print('How brave are you?')
+    print('1. Very brave! I am only scared of x-large, hairy spiders.')
+    print('2. Reasonably brave. I am only occasionaly riddled with self doubt and fear of public humilation.')
+    print('3. I am already scared...')        
