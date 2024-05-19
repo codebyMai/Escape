e6f8e24ef1b0000066881159d33627c81c8dfa2a
codebyMai <158331419+codebyMai@users.noreply.github.com>
2024-05-19 14:17:53 +0100

    add function allowing user to move

--- run.py
+++ run.py
@@ -51,7 +51,9 @@ def difficulty():
             level = int(input('Enter 1,2 or 3 depending how brave are you feeling.\n'))
             if level == 1:
                 Board.width  = 20
-                Board.height  = 20 
+                Board.height  = 20
+                key_x = randint(0, Board.width)
+                key_y = randint(0, Board.height)
                 break
             elif level == 2:
                 Board.width  = 15
@@ -69,40 +71,7 @@ def difficulty():
     clear()
     return Board.height, Board.width
 
-"""def difficulty():
-
-    Sets size of the board depending on the user choice of difficulty.
-    while True:
-        try:
-            level = int(input('Enter 1,2 or 3 depending how brave are you feeling.\n'))
-            
-            if level == 1 or 2 or 3:
-                choice()
-            else:
-                print('You do not stand a chance in this game if you cannot follow simple instructions.')
-                continue
-        except ValueError:   
-            print('You ')
-            
-    
-def choice():
-    if level := 1:
-        Board.width  = 20
-        Board.height  = 20 
-
-    elif level := 2:
-        Board.width  = 15
-        Board.height  = 15
-
-    else:
-        level == 3
-        Board.width  = 10
-        Board.height  = 10
-
-    else:
-        if level != 1 or 2 or 3:
-            print('You do not stand a chance in this game if you cannot follow simple instructions.')
-            level = input('Enter 1, 2 or 3:\n')"""                  
+        
 
 def intro():
     """
@@ -127,16 +96,16 @@ def intro():
     time.sleep(2)
     clear() 
 
-def game():
-    """
+"""def game():
+
     Sets game variables.
-    """
+    
     player_x  = 0
     player_y  = 0
     steps = 0
     key_x = randint(0, Board.width)
     key_y = randint(0, Board.height)
-    key_found  = False
+    key_found  = False"""
 
 def before():
     """
@@ -144,7 +113,53 @@ def before():
     """
     #before_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
 
+def moves():
+    
+    """
+    Player movements and steps addition"
+    """
+    player_x  = 0
+    player_y  = 0
+    steps = 0
+    key_x = randint(0, Board.width)
+    key_y = randint(0, Board.height)
+    key_found  = False
+    while not key_found:
+        before()
+        steps += 1
+        move = input('Quick! Where do you want to go?')
+        match move.lower():
+            case 'w':
+                player_y += 1
+                if player_y > Board.height:
+                    print('Oops! You have just crashed into the wall!')
+                    player_y = Board.game_height
 
+            case 's':
+                player_y -= 1
+                if player_y < 0:
+                    print(f'@\u21af\u2737! That hurt! You cannot walk through walls.')
+                    player_y = 0
+
+            case 'a':
+                player_x -= 1
+                if player_x < 0:
+                    print('If you are not careful, you are going to end up concussed as well as kidnapped!')
+                    player_x = 0
+
+            case 'd':
+                player_x += 1
+                if player_x > Board.width:
+                    print('You hit the wall!')
+                    player_x = Board.game_width
+
+            case 'q':
+                print ('Having just remembered that you are terribly scared of the dark, you wake up and realise it was all just a bad dream!')
+                quit()
+
+            case _:
+                print ('You can only move using W/S/A/D keys!')
+                continue   
 
 if __name__ == '__main__':
     welcome_screen()
