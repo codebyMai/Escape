ad67a0a1547791639d85452f35c5bae5975a0d13
codebyMai <158331419+codebyMai@users.noreply.github.com>
2024-05-21 14:39:58 +0100

    fix punctuation after passing through linter

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ from random import randint
 from math import sqrt
 from google.oauth2.service_account import Credentials
 from tabulate import tabulate
-import os, sys, time
+import os
+import sys
+import time
 import pyfiglet
 import gspread
 
@@ -22,11 +24,13 @@ SHEET = GSPREAD_CLIENT.open("hall_of_fame")
 
 leaders = SHEET.worksheet('leaderboard')
 
+
 def clear():
     """
     Cleans terminal
     """
     os.system('cls')
+
 
 def typingPrint(text):
     """
@@ -37,18 +41,21 @@ def typingPrint(text):
         sys.stdout.flush()
         time.sleep(0.01)
 
+
 def welcome_screen():
     """
     Prints title of the game and asks user to choose difficulty level
     """
-    title = pyfiglet.figlet_format('Escape',font= 'doom')
-    typingPrint(title)    
+    title = pyfiglet.figlet_format('Escape', font='doom')
+    typingPrint(title)
 
-    print('Warning! This game is going to make you face your deepest fears...\n')
+    print('Warning!')
+    print('This game is going to make you face your deepest fears...\n')
     print('How brave are you?')
     print('1. Very brave! I am only scared of x-large, hairy spiders.')
-    print('2. Reasonably brave. I am only occasionaly riddled with self doubt and fear of public humilation.')
-    print('3. I am already scared...') 
+    print('2. Reasonably brave. Occasionaly riddled with self doubt.')
+    print('3. I am already scared...')
+
 
 def difficulty():
     """
@@ -63,34 +70,35 @@ def difficulty():
     global level
     while True:
         try:
-            level = int(input('Enter 1,2 or 3 depending how brave are you feeling.\n'))
+            level = int(input('Enter 1,2 or 3 depending how brave you are.\n'))
             if level == 1:
-                game_width  = 20
-                game_height  = 20
+                game_width = 20
+                game_height = 20
                 global spider_x
                 global spider_y
                 spider_x = randint(0, game_width)
                 spider_y = randint(0, game_height)
                 break
             elif level == 2:
-                game_width  = 15
-                game_height  = 15
+                game_width = 15
+                game_height = 15
                 global doubt_x
                 global doubt_y
                 doubt_x = randint(0, game_width)
                 doubt_y = randint(0, game_height)
                 break
             elif level == 3:
-                game_width  = 10
-                game_height  = 10
+                game_width = 10
+                game_height = 10
                 break
             else:
                 clear()
                 raise ValueError()
-        except ValueError as e_rr:
-            print('You do not stand a chance in this game if you cannot follow simple instructions.')
+        except ValueError:
+            print('To survive in this game you need to follow instructions!')
     clear()
-        
+
+
 def before():
     """
     Measures distance to the key before move to enable hints
@@ -100,7 +108,8 @@ def before():
     global key_y
     key_y = randint(0, game_height)
     global before_move
-    before_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)      
+    before_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
+
 
 def intro():
     """
@@ -109,8 +118,10 @@ def intro():
     global name
     typingPrint('It is cold, pitch black and very, very quiet.\n')
     time.sleep(1)
-    typingPrint('With horror you realise you have no idea where you are or how you got here...\n')
-    time.sleep(1)       
+    typingPrint('With horror you realise you have no idea where you are...\n')
+    time.sleep(1)
+    typingPrint('Or how you got here...\n')
+    time.sleep(1)
     typingPrint('Suddenly you hear a hushed voice in the dark.\n')
     time.sleep(1)
     print('Who are you? What is your name?\n')
@@ -118,16 +129,16 @@ def intro():
     if not name.isalpha():
         print("Only letters are allowed!")
         name = input('Enter your name.\n')
-    print(f'{name.capitalize()} you are our only chance to get out of here alive!\n')
+    print(f'{name.capitalize()} only you can save us!\n')
     time.sleep(1)
     print('There is a key somewhere on the ground.\n')
     time.sleep(1)
     print('You must find it to get out!')
     time.sleep(1)
-    
     clear()
-    moves() 
+    moves()
     return name
+
 
 def moves():
     """
@@ -138,12 +149,10 @@ def moves():
     global player_y
     player_y = 0
     global steps
-    
-    steps = 0 
-    key_found  = False 
+    steps = 0
+    key_found = False
     before()
     while not key_found:
-       
         steps += 1
         move = input('Quick! Where do you want to go?\n')
         match move.lower():
@@ -156,13 +165,13 @@ def moves():
             case 's':
                 player_y -= 1
                 if player_y < 0:
-                    print(f'@\u21af\u2737! That hurt! You cannot walk through walls.')
+                    print(f'@\u21af\u2737! You cannot walk through walls.')
                     player_y = 0
 
             case 'a':
                 player_x -= 1
                 if player_x < 0:
-                    print('If you are not careful, you are going to end up concussed as well as kidnapped!')
+                    print('You are going to end up concussed!')
                     player_x = 0
 
             case 'd':
@@ -172,12 +181,12 @@ def moves():
                     player_x = game_width
 
             case 'q':
-                print ('Having just remembered that you are terribly scared of the dark, you wake up and realise it was all just a bad dream!')
+                print('Suddenly you realise it was all just a bad dream!')
                 quit()
 
             case _:
-                print ('You can only move using W/S/A/D keys!')
-                continue  
+                print('You can only move using W/S/A/D keys!')
+                continue
         after()
 
         if level == 1 and player_x == spider_x and player_y == spider_y:
@@ -185,13 +194,13 @@ def moves():
             print('You recoil in horror and end up back where you started!')
             player_x = 0
             player_y = 0
-       
         if level == 2 and player_x == doubt_x and player_y == doubt_y:
             print('Suddenly you feel riddled with self doubt!')
             print('You stagger back to where you started!')
             player_x = 0
             player_y = 0
-        
+
+
 def after():
     """
     Distance from the key and hints
@@ -199,18 +208,18 @@ def after():
     global before_move
     after_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
     if before_move > after_move:
-       print('You are getting closer!')
+        print('You are getting closer!')
     else:
-       print('You are moving away from the key!')
-
-    before_move = after_move 
+        print('You are moving away from the key!')
+    before_move = after_move
     end_game()
+
 
 def end_game():
     """
-    Displays message when key found and update the step count 
+    Displays message when key found and update the step count
     """
-    free = pyfiglet.figlet_format('Free!',font= 'doom')
+    free = pyfiglet.figlet_format('Free!', font='doom')
     if player_x == key_x and player_y == key_y:
         print('You found the \U0001F511 ! You are')
         print(free)
@@ -218,14 +227,16 @@ def end_game():
         update_score()
         end_game_choice()
 
+
 def end_game_choice():
     """
     Displays choice of options
-    """            
+    """
     print('To play againg - type 1.')
     print('To check "Hall of Fame" scores - type 2.')
     print('To quit - type 3.')
-    options() 
+    options()
+
 
 def update_score():
     """
@@ -234,6 +245,7 @@ def update_score():
     user = name.capitalize()
     update = [user, steps]
     leaders.insert_row(update, 2)
+
 
 def results():
     """
@@ -254,9 +266,10 @@ def results():
             else:
                 clear()
                 raise ValueError()
-        except ValueError as e_rr:
+        except ValueError:
             print(f'You must press 1 to go back.')
-    
+
+
 def options():
     """
     Options offered to user at the end of game
@@ -277,12 +290,11 @@ def options():
             else:
                 clear()
                 raise ValueError()
-        except ValueError as e_rr:
+        except ValueError:
             print('Try again. Press 1, 2 or 3.')
+
 
 if __name__ == '__main__':
     welcome_screen()
     difficulty()
     intro()
-     
-       
